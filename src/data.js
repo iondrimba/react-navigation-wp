8cf17b6e355de61d827245bb4729e31970f2a62b
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2017-02-03 16:41:41 -0200

    open graph

--- src/data.js
+++ src/data.js
@@ -12,54 +12,6 @@ let data = {
         "path": "option",
         "color": "purple"
       },
-      "option 1": {
-        "model": {
-          "description": "option 1",
-          "title": "option 1",
-          "path": "option-1",
-          "color": "#eae62a"
-        },
-        "option 2": {
-          "model": {
-            "description": "option 2",
-            "title": "option 2",
-            "path": "option-2",
-            "color": "#14ab6d"
-          }
-        },
-        "option 3": {
-          "model": {
-            "description": "option 3",
-            "title": "option 3",
-            "path": "option-3",
-            "color": "#14ab6d"
-          }
-        }
-      },
-      "option 4": {
-        "model": {
-          "description": "option 4",
-          "title": "option 4",
-          "path": "option-4",
-          "color": "#eae62a"
-        },
-        "option 5": {
-          "model": {
-            "description": "option 5",
-            "title": "option 5",
-            "path": "option-5",
-            "color": "purple"
-          }
-        },
-        "option 6": {
-          "model": {
-            "description": "option 6",
-            "title": "option 6",
-            "path": "option-6",
-            "color": "purple"
-          }
-        }
-      }
     },
     "option-7": {
       "model": {
@@ -68,38 +20,6 @@ let data = {
         "path": "option-7",
         "color": "purple"
       },
-      "option 8": {
-        "model": {
-          "description": "option 8",
-          "title": "option 8",
-          "path": "option-8",
-          "color": "#4196e2"
-        },
-        "option 9": {
-          "model": {
-            "description": "option 9",
-            "title": "option 9",
-            "path": "option-9",
-            "color": "#e82f61"
-          }
-        },
-        "option 10": {
-          "model": {
-            "description": "option 10",
-            "title": "option 10",
-            "path": "option-10",
-            "color": "#e82f61"
-          }
-        }
-      },
-      "option 11": {
-        "model": {
-          "description": "option 11",
-          "title": "option 11",
-          "path": "option-11",
-          "color": "#4196e2"
-        }
-      }
     },
     "option 12": {
       "model": {
@@ -116,6 +36,37 @@ let data = {
         "path": "option-13",
         "color": "purple"
       }
+    }
+  },
+  "/2": {
+    "model": {
+      "title": "Escolha uma opção 2",
+      "path": "/",
+      "color": "yellow"
+    },
+    "option 21": {
+      "model": {
+        "description": "option 21",
+        "title": "option 21",
+        "path": "option-21",
+        "color": "yellow"
+      },
+    },
+    "option 22": {
+      "model": {
+        "description": "option 22",
+        "title": "option 22",
+        "path": "option-22",
+        "color": "yellow"
+      },
+    },
+    "option 23": {
+      "model": {
+        "description": "option 23",
+        "title": "option 23",
+        "path": "option-23",
+        "color": "yellow"
+      },
     }
   }
 }
